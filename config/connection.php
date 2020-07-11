80a36a70f44c3a3e096130a5f42c10f0c85e2d92
Ricky Sukma <rickysukma9a@gmail.com>
2020-07-11 18:59:12 +0700

    perbaikan harga checkin

--- config/connection.php
+++ config/connection.php
@@ -9,19 +9,24 @@ DB::$error_handler = false;
 DB::$throw_exception_on_error = true;
 
 function harga($id_pesanan){
+    
     $data = DB::queryFirstRow("SELECT a.*, b.harga FROM tbl_pemesanan a JOIN tbl_layanan b ON b.id_layanan = a.id_layanan WHERE a.id_pemesanan = '$id_pesanan'");
 
     extract($data);
 
     if($checkin != ''){
-        $checkin = strtotime($checkin);
-        $checkout = strtotime($checkout);
+        $checkin = date_create($checkin);
+        $checkout = date_create($checkout);
 
-        $timedif = abs($checkout - $checkout);
-        $hari = $timedif/86400;
-
+        $timedif = date_diff($checkin,$checkout);
+        $hari = $timedif->format("%d");;
+        if($hari == 0){
+            $hari = 1;
+        }
         $hari = intval($hari);
         $harga = $hari * $harga;
+    }else{
+        $harga = $harga;
     }
 
     return $harga;
